72a986a4db933f49365088605737416b7d057a13
ic20b080 <83973552+ic20b080@users.noreply.github.com>
2022-02-19 03:43:39 +0100

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
 echo install service
+cd ~/.config/systemd/user/
 systemctl --user enable boot_pi.service
+cd ../../..
 
 echo check ovs
 cd openvswitch-2.16.2/datapath/linux/
-modprobe openvswitch
+sudo modprobe openvswitch
 cd ../..
 
 echo add files
-touch /usr/local/etc/ovs-vswitchd.conf
-mkdir -p /usr/local/etc/openvswitch
-ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
+sudo touch /usr/local/etc/ovs-vswitchd.conf
+sudo mkdir -p /usr/local/etc/openvswitch
+sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
 
-/etc/init.d/superscript
+echo start ovs
+sudo /etc/init.d/superscript
 
 echo show ovs
-ovs-vsctl show
+sudo ovs-vsctl show
 
 echo create ovs bridge
-ovs-vsctl add-br br0
+sudo ovs-vsctl add-br br0
 
 echo add interfaces to openflow
-ovs-vsctl add-port br0 eth0
-ovs-vsctl add-port br0 eth1
-ovs-vsctl add-port br0 eth2
-ovs-vsctl add-port br0 eth3
+sudo ovs-vsctl add-port br0 eth0
+sudo ovs-vsctl add-port br0 eth1
+sudo ovs-vsctl add-port br0 eth2
+sudo ovs-vsctl add-port br0 eth3
 
 echo add openflow controller
-ovs-vsctl set-controller br0 tcp:192.168.8.18:6633
-ovs-vsctl set bridge br0 protocols=OpenFlow14
+sudo ovs-vsctl set-controller br0 tcp:192.168.8.18:6633
+sudo ovs-vsctl set bridge br0 protocols=OpenFlow14
+
+echo reboot
+reboot
